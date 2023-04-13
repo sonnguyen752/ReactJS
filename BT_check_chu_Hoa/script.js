0aea71993d57d7f9a0a47b5ca3aaef18fe33cfe5
sonnguyen752 <sonnguyen752@gmail.com>
2023-04-13 08:13:57 +0700

    bai tap kiem tra chu cai dau tien la chu Hoa

---
+++ BT_check_chu_Hoa/script.js
@@ -0,0 +1,8 @@
+function KiemTraChuHoa(){
+    const regexp=/^[A-Z]/;
+    if(regexp.test('sDGFvbnx')){
+        console.log('Co Chu Hoa o dau')
+    }else{
+        console.log("Khong co chu hoa dau cau")
+    }
+    }
