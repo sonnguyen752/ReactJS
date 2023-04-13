a8f68c129400a6fd8a0d38ba96925f6424bfb337
sonnguyen752 <sonnguyen752@gmail.com>
2023-04-13 08:36:04 +0700

    Sử dụng Set thao tác với dữ liệu

--- Use_Seter/script.js
+++ Use_Seter/script.js
@@ -1,20 +1,9 @@
-var DS = [
-    {'name':'Cho'}, 
-    {'name':'meo'},
-    {'name':'lon'},
-    // {'name':'Trau'},
-    ]
-    
-//them vao danh sach
-    // DS.push('Trau')
-    // console.log(DS)
-// kiem tra danh sach
-
-
-
-DS.filter(nm => nm.name =='Trau')
-    console.log('da co trong DS')
-DS.filter(nm => nm.name !='Trau')
-    console.log('Chua co trong DS')   
-// }));
-// console.log(tieu_hoc)
+animals = ["cat","dog","muose"]
+// kiem tra da co trong DS hay chua
+console.log(animals.some(animal => animal === 'cat'))
+// Them vao danh sach
+animals.push('Big')
+console.log(animals)
+// xoa khoi danh sach
+const newAnimals = animals.filter(item=>item !== 'cat')
+console.log(newAnimals)
